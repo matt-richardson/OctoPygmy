fb76621cb5b7ba81e1edb00f3b14e298dc5d9476
David Roberts <david.roberts63@gmail.com>
2015-03-07 15:59:45 -0600

    showItems tests

--- spec/commonpygmy.js
+++ spec/commonpygmy.js
@@ -20,4 +20,39 @@ describe("commonpygmy", function() {
 		});
 
 	});
+
+	describe("showItems", function() {
+		var allIds = '';
+		var idsToShow = '';
+		var allItems = '';
+
+		beforeEach(function() {
+			allIds = ['1','2','3','4'];
+			idsToShow = ['2','4'];
+			allItems = {
+				'1': { style: { display: ''} },
+				'2': { style: { display: ''} },
+				'3': { style: { display: ''} },
+				'4': { style: { display: ''} }
+			}
+			spyOn(document,'getElementById').and.callFake(function(id) {
+				return allItems[id];
+			});
+		});
+
+		it("shows the specified items", function() {
+
+			commonpygmy.showItems(allIds, idsToShow, 'show', 'hide');
+
+			expect(allItems['2'].style.display).toEqual('show');
+			expect(allItems['4'].style.display).toEqual('show');
+		});
+
+		it ("hides the items not specified", function() {
+			commonpygmy.showItems(allIds, idsToShow, 'show', 'hide');
+
+			expect(allItems['1'].style.display).toEqual('hide');
+			expect(allItems['3'].style.display).toEqual('hide');
+		});
+	});
 });
