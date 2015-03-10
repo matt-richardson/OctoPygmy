630379391c97cbcb766f12f7c10f91136d7ab153
prog2dr <david.roberts@americanfidelity.com>
2015-03-10 16:27:19 -0500

    Remove logging for performance. Will revisit with js buildtool

--- src/commonpygmy.js
+++ src/commonpygmy.js
@@ -18,7 +18,6 @@ var commonpygmy = {
 
 	showItems: function(allIds, idsToShow, showStyle, hideStyle)
 	{
-		console.log("Showing: " + idsToShow);
 		var showAll = idsToShow == commonpygmy.allItemsValue || idsToShow == ''
 
 		for(var id of allIds)
@@ -26,12 +25,10 @@ var commonpygmy = {
 			var item = document.getElementById(id);
 			if (showAll || idsToShow.indexOf(id) >= 0)
 			{
-				console.debug("Showing " + id);
 				item.style.display = showStyle;
 			} 
 			else 
 			{
-				console.debug("Hiding " + id);
 				item.style.display = hideStyle;
 			}
 		}
