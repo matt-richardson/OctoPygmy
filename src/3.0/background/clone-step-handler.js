429a152f9f41b6cecc12369a87de5df9bf167eab
Matt Richardson <matt.richardson@asos.com>
2015-12-04 16:55:37 +0000

    Refactoring in preparation of adding tests

--- src/3.0/background/clone-step-handler.js
+++ src/3.0/background/clone-step-handler.js
@@ -1,5 +1,38 @@
 pygmy3_0.cloneStepHandler = (function() {
 
+	function handlePutResponse(status, response, sendResponse, stepId, deploymentProcessId)
+	{
+		console.debug("Received put reponse");
+		console.debug(response);
+
+		var result = JSON.parse(response);
+		var response;
+		if (status == 200 ) {
+			response = { message: 'clone-step-response', properties: { status: 'success', stepId: stepId, deploymentProcessId: deploymentProcessId }};
+		}
+		else {
+			response = { message: 'clone-step-response', properties: { status: 'failure', errorMessage: result.ErrorMessage, errors: result.Errors }}
+		}
+		console.log('sending clone-step-response: ' + response);
+		sendResponse(response);
+	}
+
+	function handleGetResponse(response, sendResponse, stepId, deploymentProcessId, url)
+	{
+		var step = response.Steps.filter(function(step) { return step.Id == stepId })[0];
+		var newStep = JSON.parse(JSON.stringify(step));
+		newStep.Id = "";
+		newStep.Name = newStep.Name + " - clone"
+		for(i = 0; i < newStep.Actions.length; i++) {
+			newStep.Actions[i].Id = "";
+			newStep.Actions[i].Name = newStep.Actions[i].Name + " - clone"
+		}
+		response.Steps.push(newStep);
+
+		nanoajax.ajax({url: url, method: "PUT", body: JSON.stringify(response)}, 
+			function (status, response) { handlePutResponse(status, response, sendResponse, stepId, deploymentProcessId); });
+	}
+
 	function cloneStep(request, sender, sendResponse)
 	{
 		if (request.message == 'clone-step')
@@ -10,33 +43,7 @@ pygmy3_0.cloneStepHandler = (function() {
 			var octopusRoot = sender.tab.url.substring(0,sender.url.indexOf('/app'));
 
 			var url = octopusRoot + "/api/deploymentprocesses/" + deploymentProcessId;
-			getJsonResponse(url, function(response) {
-				var step = response.Steps.filter(function(step) { return step.Id == stepId })[0];
-				var newStep = JSON.parse(JSON.stringify(step));
-				newStep.Id = "";
-				newStep.Name = newStep.Name + " - clone"
-				for(i = 0; i < newStep.Actions.length; i++) {
-					newStep.Actions[i].Id = "";
-					newStep.Actions[i].Name = newStep.Actions[i].Name + " - clone"
-				}
-				response.Steps.push(newStep);
-
-				nanoajax.ajax({url: url, method: "PUT", body: JSON.stringify(response)}, function(status, response){
-					console.debug("Received put reponse:" + url);
-					console.debug(response);
-
-					var result = JSON.parse(response);
-					var response;
-					if (status == 200 ) {
-						response = { message: 'clone-step-response', properties: { status: 'success', stepId: stepId, deploymentProcessId: deploymentProcessId }};
-					}
-					else {
-						response = { message: 'clone-step-response', properties: { status: 'failure', errorMessage: result.ErrorMessage, errors: result.Errors }}
-					}
-					console.log('sending clone-step-response: ' + response);
-					sendResponse(response);
-				});
-			});
+			getJsonResponse(url, function (response) { handleGetResponse(response, sendResponse, stepId, deploymentProcessId, url); } );
 
 			return true; // see https://developer.chrome.com/extensions/runtime#event-onMessageExternal
 		}
