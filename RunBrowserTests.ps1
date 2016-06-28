c71a529eba5718692cf9a9ddbbb5783b79fa32b2
David Roberts <david.roberts63@gmail.com>
2016-06-28 14:18:01 -0500

    Going back to the jasmine node binary

--- RunBrowserTests.ps1
+++ RunBrowserTests.ps1
@@ -54,7 +54,7 @@ if($failed -ge $max)
 }
 
 Write-Host "Running browser tests..."
-& node node_modules/jasmine-node/lib/jasmine-node/cli.js --captureExceptions --verbose spec/browser-tests
+& .\node_modules\.bin\jasmine-node --capture-exceptions --verbose spec/browser-tests
 
 Write-Host "Stopping test VM..."
 Stop-AzureRMVM -ResourceGroupName $VMResourceGroupName -Name $VMName -Force | Out-Null
