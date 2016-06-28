5e04977ab5b3064a2330fc2abae4bbe15c99f18f
David Roberts <david.roberts63@gmail.com>
2016-06-28 00:27:03 -0500

    First browser test complete!

---
+++ spec/browser-tests/dashboard-collapser.spec.js
@@ -0,0 +1,105 @@
+var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+var SauceLabs = require("saucelabs");
+var username = process.env.SauceLabUsername;
+var accessKey = process.env.SauceLabAccessKey;
+var octopusUrl = process.env.OctopusUrl;
+var octopusPassword = process.env.OctopusPassword;
+var saucelabs = new SauceLabs({
+      username: username,
+      password: accessKey
+    });
+
+jasmine.getEnv().defaultTimeoutInterval = 100000;
+
+describe('Dashboard collapser', function() {
+    var driver = null;
+    
+    beforeEach(function(done) {
+        driver = new webdriver.Builder().
+            withCapabilities({
+                'name': 'Spec run in node',
+                'build': '1.51.15',
+                'browserName': 'chrome',
+                'platform': 'Windows 10',
+                'version': '43.0',
+                'username': username,
+                'accessKey': accessKey,
+                'prerun': 'https://gist.githubusercontent.com/davidroberts63/b7c01e7aa945cde9064aca04e947288a/raw/269145793cb19a6081b5c65e9bddd421ded73234/GetExtension.bat',
+                "chromeOptions": {
+                    "args": [ 
+                        "--load-extension=C:\\bluefin-extension\\src",
+                        "start-maximized",
+                        "disable-webgl",
+                        "blacklist-webgl",
+                        "blacklist-accelerated-compositing",
+                        "disable-accelerated-2d-canvas",
+                        "disable-accelerated-compositing",
+                        "disable-accelerated-layers",
+                        "disable-accelerated-plugins",
+                        "disable-accelerated-video",
+                        "disable-accelerated-video-decode",
+                        "disable-gpu",
+                        "disable-infobars",
+                        "test-type"
+                    ]
+                    },
+            }).
+            usingServer("http://" + username + ":" + accessKey +
+              "@ondemand.saucelabs.com:80/wd/hub").
+            build();
+
+        var spec = jasmine.getEnv().currentSpec;
+        driver.getSession().then(function(sessionid) {
+            driver.sessionID = sessionid.id_;
+        })
+        .then(new Promise(function(resolve, reject) {
+            saucelabs.updateJob(driver.sessionID, {
+                name: spec.suite.description + ' ' + spec.description
+            }, resolve);
+        }))
+        .then(closeOptionsTab)
+        .then(login)
+        .then(done);
+    });
+
+    afterEach(function(done) {
+        var spec = jasmine.getEnv().currentSpec;
+        saucelabs.updateJob(driver.sessionID, {
+            passed: spec.results_.totalCount == spec.results_.passedCount,
+            name: spec.suite.description + ' ' + spec.description
+        }, function () {});
+        driver.quit().then(done);
+    });
+    
+    function closeOptionsTab() {
+        return driver.getAllWindowHandles().then(function(handles) {
+            driver.switchTo().window(handles[1]);
+            driver.close();
+            driver.switchTo().window(handles[0]);
+        });
+    }
+
+    function login(done) {
+        driver.get(octopusUrl);
+        driver.sleep(1000);
+        driver.findElement(By.id("inputUsername")).sendKeys("AdministratorJoe");
+        driver.findElement(By.id("inputPassword")).sendKeys(octopusPassword);
+        driver.findElement(By.css("button.btn.btn-success[type='submit']")).click();
+        return driver.sleep(3000).then();
+    }
+    
+    it('should be shown', function (done) {
+        driver.sleep(500).then(done);
+        driver.findElement(By.css("select#foobar-project-chooser"))
+            .then(done);
+    });
+    /*
+    it('should contain all project groups', function(done) {
+        var chooser = driver.findElement(By.css("select#project-chooser"));
+        chooser.click();
+        chooser.findElements(By.tagName("OPTION"))[1].isDisplayed()
+            .then(function() { done(); });
+    });
+    */
+});
