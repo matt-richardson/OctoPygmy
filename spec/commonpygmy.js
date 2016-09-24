b5a37ba127fa980cc6c17539ab39bda8b69367dd
Matt Richardson <matt@redgumtech.com.au>
2016-09-25 07:50:31 +1000

    Include commonpygmy tests into test run
    And fix failing tests where octopygmy-id now starts with a ‘z’

--- spec/commonpygmy.js
+++ spec/commonpygmy.js
@@ -25,11 +25,11 @@ describe("commonpygmy", function() {
 		var idsToShow = '';
 		// When setting the style.display. Be sure to use valid values.
 		// It runs against the XHTML parser.
-		var nodeHtml = '<div id="z" octopygmy-id="a">&nbsp;</div>' +
-						'<div id="x" octopygmy-id="b">&nbsp;</div>' +
-						'<div id="y" octopygmy-id="c">&nbsp;</div>' +
-						'<div id="w" octopygmy-id="d">&nbsp;</div>' +
-						'<div octopygmy-id="d">&nbsp;</div>';
+		var nodeHtml = '<div id="z" octopygmy-id="za">&nbsp;</div>' +
+						'<div id="x" octopygmy-id="zb">&nbsp;</div>' +
+						'<div id="y" octopygmy-id="zc">&nbsp;</div>' +
+						'<div id="w" octopygmy-id="zd">&nbsp;</div>' +
+						'<div octopygmy-id="zd">&nbsp;</div>';
 		var nodeSegment = '';
 
 		beforeEach(function() {
