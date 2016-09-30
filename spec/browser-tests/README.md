ad737a950eb761e5a86863fa6452d7c4d9c22352
David Roberts <david.roberts63@gmail.com>
2016-09-29 23:16:18 -0500

    Load data and setup scripts. Along with readme update.

--- spec/browser-tests/README.md
+++ spec/browser-tests/README.md
@@ -22,6 +22,60 @@
 ## Running tests locally
 Run the `RunBrowserTestsLocally.ps1` powershell script, be sure you've run `npm install`. You will also need a copy of the chrome driver in your PATH. You can download it from [Google](https://sites.google.com/a/chromium.org/chromedriver/). The latest one should be okay. Just be sure you put the `chromedriver.exe` (that's all there is) somewhere in your PATH. You can just put it in the folder you execute the script from as well. Just don't commit it. When running that script you will need to provide the `-octopusUrl`. Typically it'll be `http://localhost` but you may need to specify the port in that url if you changed that.
 
+## Loading test data into the Octopus Deploy server.
+**Be careful** this part destroys data. Make sure you are connecting to a test server and database. The `Load-Octopus.ps1` powershell script in `browser-tests\setup-scripts` will load the test data that the browser tests rely on.
+
+Example call of load script:
+```
+& .\Load-Octopus.ps1 `
+   -OctopusRootUrl "http://localhost:8034/"`
+   -SqlServerInstance "SQLExpress"`
+   -DatabaseName "Octopus34"`
+   -ServiceName "OctopusDeploy: OctopusServer34"`
+   -OctopusAdminUsername "JoeAdministrator"`
+   -OctopusAdminPassword "Password 123."`
+   -GeneratedDataFilePath ".\gendata.json"`
+   -DatabaseBackupFilePath "C:\Backups\Octopus34Loaded.bak"`
+   -TentacleInstallPath "C:\Program Files\Octopus Deploy\Tentacle"`
+   -ExportPath "C:\Backups\OctopusExport34"`
+   -MigratorPath "C:\Program Files\Octopus Deploy\Octopus-3.4.9\Octopus.Migrator.exe"`
+   -InstanceName "OctopusServer34"
+```
+
+The first thing the script does is restore a database backup as specified in the `-DatabaseBackupFilePath` parameter. If that file does not exist it will create the folder (if needed) and then make a backup of the database to that location. So be sure SQL Server has access to the path you specify. It does this so you can re-load the data multiple times without duplicating anything. 
+
+> **NOTE** The load script assumes you are using a local instance of SQL Server Express. If not you will need to change the connection string created within the script. This will change later to become more flexible.
+>
+> If you are connecting to a remote SQL Server then the backup will be on the remote server, not your local machine.
+
+Once the initial database has been restored the test data is loaded in one of two ways.
+- Pull Envrionment, Target and Project names from `browser-tests\setup-scripts\gendata.json` and use the Octopus Deploy REST api to create the items appropriately. If this is done then an export of the data is made using the Octopus.Migrator export command line after all the items have been created.
+- Import a prior Octopus.Migrator export.
+
+The JSON export of the data will be put in the path specified by the `-ExportPath` parameter. As in the first way, if that export does not already exist the data is loaded via REST API calls and then an export is made to that location. If that location does exist then it just imports that. You can use this to add data to try out tests. But other people/builds will use the REST API path initially.
+
+The second way is primarily to allow for quick resets of data after the tests that change data.
+
+You need to have a Tentacle installed. The `-TentacleInstallPath` is used so that the load script can get the thumbprint of the tentacle for use in created valid targets.
+
+### The SetupOctopusDeployServer.ps1 script
+This is intended to be used for testing **multiple** versions of Octopus Deploy on one machine. Be **very** carful with this. Only run it on a VM or other test machine as it messes with the registry and has the potential to mess things up.
+
+Having said that.
+
+It installs [Chocolatey](https://chocolatey.org/). Using chocolatey it installs the following:
+- SQL Server 2014 Express
+- Octopus Deploy Tentacle 3.4.0
+- Octopus Deploy Server 3.4.0
+- Messes with the registry to make MsiExec think Octopus Deploy is not installed.
+- Octopus Deploy Server 3.4.9
+
+Each version of the Octopus Deploy server is installed to its own folder so they can run side by side. They use a separate database each but connect to the same Tentacle. The port number for each install of Octopus Deploy Server is different according to the version number. For example `http://localhost:8034` will bring up the 3.4.9 server web app.
+
+> If you make changes you will need to use the same changes (port, instance,...) in the load script and tests.
+
+You will need to provide a license file for use. Specify the path to it with the `-licenseFilePath` parameter to the script.
+
 ## Naming
 Name the browser test spec files in the following manner: `feature`.spec.js. Feature should be the same name as the source file that implements the feature.
 
---
+++ spec/browser-tests/setup-scripts/SetupOctopusDeployServer.ps1
@@ -0,0 +1,116 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    $licenseFilePath
+)
+
+$ErrorActionPreference = "STOP"
+
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
+Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
+
+Install-ChocolateyPackage `
+    -PackageName 'SQLServerExpress2014' `
+    -FileType 'exe' `
+    -SilentArgs "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE" `
+    -Url 'https://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2032BIT/SQLEXPR_x86_ENU.exe' `
+    -Url64bit 'https://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/Express%2064BIT/SQLEXPR_x64_ENU.exe' `
+    -Checksum64 '8F712FAEFEE9CEF1D15494C9D6CF5AD3B45EC06D0B2C247F8384A221BAAADDA7' `
+    -Checksumtype64 'SHA256' `
+    -ValidExitCodes @(0,3010) `
+    --allow-empty-checksums-secure
+
+Install-ChocolateyPackage `
+    -PackageName 'OctopusDeploy.Tentacle' `
+    -FileType 'msi' `
+    -SilentArgs @('INSTALLLOCATION="C:\Program Files\Octopus Deploy\Tentacle"','/quiet') `
+    -UseOnlyPackageSilentArguments `
+    -Url 'https://download.octopusdeploy.com/octopus/Octopus.Tentacle.3.4.0.msi' `
+    -Url64Bit 'https://download.octopusdeploy.com/octopus/Octopus.Tentacle.3.4.0-x64.msi' `
+    -Checksum64 '4339B29B3B0A5BFA9EFB8AE7696724B2231AA45BC5EA89721D6BD2E56AF34EB0' `
+    -ChecksumType64 'SHA256' `
+
+Install-ChocolateyPackage `
+    -PackageName 'OctopusDeploy' `
+    -FileType 'msi' `
+    -SilentArgs @('INSTALLLOCATION="C:\Program Files\Octopus Deploy\Octopus-3.4.9"','/quiet') `
+    -UseOnlyPackageSilentArguments `
+    -Url 'https://download.octopusdeploy.com/octopus/Octopus.3.4.9.msi' `
+    -Url64bit 'https://download.octopusdeploy.com/octopus/Octopus.3.4.9-x64.msi' `
+    -Checksum '891D2FC16D7AE8CDD0F4B02587EF9498BA689D6C09BF516906795EE77135C444' `
+    -ChecksumType 'SHA256' `
+    -Checksum64 'F90C94F07B7C85084FDC67F2AB7DC142615D97175098446063728EE4B2509FF6' `
+    -ChecksumType64 'SHA256'
+
+# Clear out registry settings to allow for multiple installations.
+# NOTE: This is hacky, your registry will not reflect what is actually installed. The uninstaller won't work for both installations.
+$installedProductKey = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products -Recurse -Include InstallProperties | Where { $_.GetValue("DisplayName") -eq "Octopus Deploy Server" } | Select -ExpandProperty Name | Split-Path) -replace "HKEY_LOCAL_MACHINE","HKLM:"
+$installGuid = $installedProductKey | Split-Path -Leaf
+
+Remove-Item HKLM:\SOFTWARE\Octopus -Recurse -Force
+Remove-Item -Path $installedProductKey -Recurse -Force
+Remove-Item -Path HKLM:\Software\Classes\Installer\Products\$installGuid -Recurse -Force
+Remove-Item -Path HKLM:\Software\Classes\Installer\Features\$installGuid -Recurse -Force
+
+Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components | Where { $_.GetValue($_.Property) -like "*Octopus*" } | Remove-Item -Recurse
+Get-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\Folders `
+    | Select -ExpandProperty Property `
+    | Where { $_ -like "*Octopus*" } `
+    | %{ Remove-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\Folders -Name $_ }
+Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Where { $_.GetValue("DisplayName") -eq "Octopus Deploy Server" } | Remove-Item -Recurse -Force
+
+Install-ChocolateyPackage `
+    -PackageName 'OctopusDeploy' `
+    -FileType 'msi' `
+    -SilentArgs @('INSTALLLOCATION="C:\Program Files\Octopus Deploy\Octopus-3.3.0"','/quiet') `
+    -UseOnlyPackageSilentArguments `
+    -Url 'https://download.octopusdeploy.com/octopus/Octopus.3.3.0.msi' `
+    -Url64bit 'https://download.octopusdeploy.com/octopus/Octopus.3.3.0-x64.msi' `
+    -Checksum64 '516435AFDA219286C104403E2FFFE2858C4DC4C2B2A2DB8F22A11BFD4A0CB5B7' `
+    -ChecksumType64 'SHA256'
+
+Write-Host "Configure Octopus Deploy Server instances"
+$utf8NoBOM = New-Object System.Text.UTF8Encoding($false)
+$bytes  = $utf8NoBOM.GetBytes((GC $licenseFilePath))
+$licenseBase64 = [System.Convert]::ToBase64String($bytes)
+
+$instances = @(@{
+    server = "C:\Program Files\Octopus Deploy\Octopus-3.4.9\Octopus.Server.exe";
+    name = "OctopusServer34";
+    connectionString = "Server=.\SQLExpress;Database=Octopus34;Trusted_Connection=True;";
+    serverPort = 8034;
+    commsListenPort = 18034;
+    username = "JoeAdministrator";
+    password = "Password 123.";
+},@{
+    server = "C:\Program Files\Octopus Deploy\Octopus-3.3.0\Octopus.Server.exe";
+    name = "OctopusServer33";
+    connectionString = "Server=.\SQLExpress;Database=Octopus33;Trusted_Connection=True;";
+    serverPort = 8033;
+    commsListenPort = 18033;
+    username = "JoeAdministrator";
+    password = "Password 123.";
+})
+
+$instances | %{
+    $instance = $_
+    Write-Host "== Configuring $($instance.name) instance. " -BackgroundColor DarkCyan
+    & $instance.server create-instance --console --instance $instance.name --config "C:\$($instance.name)\OctopusServer.config"
+    & $instance.server configure --console --instance $instance.name `
+        --home "C:\$($instance.name)" `
+        --storageConnectionString $instance.connectionString `
+        --upgradeCheck False `
+        --upgradeCheckWithStatistics False `
+        --webAuthenticationMode UsernamePassword `
+        --webForceSSL False `
+        --webListenPrefixes http://localhost:$($instance.serverPort) `
+        --commsListenPort $instance.commsListenPort
+    & $instance.server database --console --instance $instance.name --create --grant="NT AUTHORITY\SYSTEM"
+    & $instance.server service --console --instance $instance.name --stop
+    & $instance.server admin --console --instance $instance.name --username $instance.username --password $instance.password
+    & $instance.server license --console --instance $instance.name --licenseBase64 $licenseBase64
+    & $instance.server service --console --instance $instance.name --install --reconfigure --start
+}
+
+Write-Host "Done"
