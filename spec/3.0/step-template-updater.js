0302d66006e1caf7b91f7583a2a9e16a3700bfb9
Matt Richardson <matt@redgumtech.com.au>
2017-03-17 14:20:14 +1100

    Add tests for disabling step template updater based on version

---
+++ spec/3.0/step-template-updater.js
@@ -0,0 +1,38 @@
+describe("step-template-updater", function() {
+  var originalConsoleDebug;
+  var originalConsoleLog;
+
+  beforeEach(function () {
+    originalConsoleDebug = console.debug;
+    originalConsoleLog = console.log;
+    console.debug = function() {};
+    console.log = function() {};
+  });
+
+  afterEach(function() {
+    console.debug = originalConsoleDebug;
+    console.log = originalConsoleLog
+  });
+
+  describe("version check", function() {
+    it("should not show for version 3.12.0", function() {
+      var content = document.createElement('div');
+      var called = false;
+      var oldValue = window.chrome;
+      window.chrome = { runtime : { onMessage : { addListener: function() { called = true; } }  }}
+      pygmy3_0.stepTemplateUpdater.observe(content, "3.12.0");
+      window.chrome = oldValue;
+      expect(called).toBe(false);
+    });
+
+    it("should show for version 3.11.1", function() {
+      var content = document.createElement('div');
+      var called = false;
+      var oldValue = window.chrome;
+      window.chrome = { runtime : { onMessage : { addListener: function() { called = true; } }  }}
+      pygmy3_0.stepTemplateUpdater.observe(content, "3.11.1");
+      window.chrome = oldValue;
+      expect(called).toBe(true);
+    });
+  });
+});
